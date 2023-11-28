98c1bc9e3a57edb170a64f0f04dc0889056a7593
Tyler Veltkamp <veltkamptyler@gmail.com>
2023-11-28 16:51:40 -0600

    if no user logged in, all pages will redirect to dash

--- main.php
+++ main.php
@@ -5,6 +5,11 @@ session_start();
 if (isset($_POST['fromAuth']) && isset($_POST['fromAuth']) == '01') {
     $_SESSION['id'] = $_POST['user_id'];
 }
+else if (!isset($_SESSION['id'])){
+    header('Location: /450Capstone/csp-450-capstone/LoginSignup/login.html');
+    die();
+}
+
 
 $id = $_SESSION['id'];
 $con = new mysqli("localhost", "root", "mysql", "therapy");
