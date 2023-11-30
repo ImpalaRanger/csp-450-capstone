8db758303c91c440f0271e6ef85f47fdb642c450
Dylan Johnson <djstealadunk+githuh@gmail.com>
2023-11-29 18:07:13 -0600

    Updated index.php

--- index.php
+++ index.php
@@ -11,71 +11,72 @@
 </head>
 <body>
 
-    <main class="container">
-        <div class="inner-container">
-            <div class="index">
-                <?php
-                    include 'main.php';
+<main class="container">
+    <div class="inner-container">
+        <div class="index">
+            <?php
+            include 'main.php';
 
-                    $stmt = $con->prepare("SELECT a.*, at.timeStart, at.timeFinish FROM appointment a
-                                        INNER JOIN appointmentTime at ON a.timeID = at.timeID
-                                        WHERE a.clientID = ? AND a.date >= CURDATE() ORDER BY a.date ASC LIMIT 5");
-                    $stmt->bind_param("i", $id);
-                    $stmt->execute();
-                    $appointments_result = $stmt->get_result();
+            $stmt = $con->prepare("SELECT a.*, at.timeStart, at.timeFinish FROM appointment a
+                                INNER JOIN appointmentTime at ON a.timeID = at.timeID
+                                WHERE a.clientID = ? AND a.date >= CURDATE() ORDER BY a.date ASC LIMIT 5");
+            $stmt->bind_param("i", $id);
+            $stmt->execute();
+            $appointments_result = $stmt->get_result();
 
-                    $stmt = $con->prepare("SELECT first_name, last_name FROM users WHERE id = ?");
-                    $stmt->bind_param("i", $id);
-                    $stmt->execute();
-                    $stmt_result = $stmt->get_result();
+            $stmt = $con->prepare("SELECT first_name, last_name FROM users WHERE id = ?");
+            $stmt->bind_param("i", $id);
+            $stmt->execute();
+            $stmt_result = $stmt->get_result();
 
-                    echo "<div class='grid-item' id='header'";
-                    if($stmt_result->num_rows > 0) {
-                        $data = $stmt_result->fetch_assoc();
-                        echo "<h2>Welcome back, " . $data['first_name'] . " " . $data['last_name'] . "!</h2><br>";
-                    }
+            echo "<div class='grid-item' id='header'>";
+            if($stmt_result->num_rows > 0) {
+                $data = $stmt_result->fetch_assoc();
+                echo "<h2>Welcome back, " . $data['first_name'] . " " . $data['last_name'] . "!</h2><br>";
+            }
+            echo "</div>";
 
-                    // Display upcoming appointments
-                    echo "<div class='grid-item' id='upcomingApts'";
-                    echo "<h3>Your upcoming appointments:</h3>";
-                    if($appointments_result->num_rows > 0) {
-                        echo "<ul>";
-                        while ($appointment = $appointments_result->fetch_assoc()) {
-                            $formattedStartTime = date('g:i A', strtotime($appointment['timeStart']));
-                            $formattedEndTime = date('g:i A', strtotime($appointment['timeFinish']));
+            // Display upcoming appointments
+            echo "<div class='grid-item' id='upcomingApts'>";
+            echo "<h3>Upcoming Appointments</h3>";
+            if($appointments_result->num_rows > 0) {
+                echo "<ul>";
+                while ($appointment = $appointments_result->fetch_assoc()) {
+                    $formattedStartTime = date('g:i A', strtotime($appointment['timeStart']));
+                    $formattedEndTime = date('g:i A', strtotime($appointment['timeFinish']));
 
-                            echo "<li>Appointment on " . $appointment['date'] . " from " . $formattedStartTime . " to " . $formattedEndTime . "</li>";
-                        }
-                        echo "</ul>";
-                    } else {
-                        echo "<p>No upcoming appointments.</p>";
-                    }
-                    echo "</div>";
+                    echo "<li>Appointment on " . $appointment['date'] . " from " . $formattedStartTime . " to " . $formattedEndTime . "</li>";
+                }
+                echo "</ul>";
+            } else {
+                echo "<p>No Upcoming Appointments.</p>";
+            }
+            echo "</div>";
 
-                    // Get the client's account balance
-                    $stmtBalance = $con->prepare("SELECT balanceAmount FROM balance WHERE userID = ?");
-                    $stmtBalance->bind_param("i", $id);
-                    $stmtBalance->execute();
-                    $balance_result = $stmtBalance->get_result();
+            // Get the client's account balance
+            $stmtBalance = $con->prepare("SELECT balanceAmount FROM balance WHERE userID = ?");
+            $stmtBalance->bind_param("i", $id);
+            $stmtBalance->execute();
+            $balance_result = $stmtBalance->get_result();
+            
+            // Get the client's account balance
+            echo "<div class='grid-item' id='actBal'>";
+            echo "<h3>Account Balance</h3>";
+            if($balance_result->num_rows > 0) {
+                $balance = $balance_result->fetch_assoc()['balanceAmount'];
+                echo "<p>Your current balance: $" . $balance . "</p>";
+            } else {
+                echo "<p>No account balance found.</p>";
+            }
+            echo "</div>";
 
-                    // Display account balance
-                    echo "<div class= 'grid-item' id='actBal'";
-                    echo "<h3>Your account balance:</h3>";
-                    if($balance_result->num_rows > 0) {
-                        $balance = $balance_result->fetch_assoc()['balanceAmount'];
-                        echo "<p>Your current balance: $" . $balance . "</p>";
-                    } else {
-                        echo "<p>Account balance not found.</p>";
-                    }
-                    echo "</div>";
+            $stmt->close();
+            $stmtBalance->close();
+            $con->close();
+            ?>
+        </div>    
+    </div>
+</main>
 
-                    $stmt->close();
-                    $stmtBalance->close();
-                    $con->close();
-                ?>
-            </div>
-        </div>
-    </main>
-    
 </body>
 </html>
